fa34751320b70f33964d318463d734e21a701a82
Thomas berteau <tob.berteau@gmail.com>
2022-03-13 15:27:09 +0100

    ~ almost working

---
+++ app/src/main/java/com/example/flashcard/screens/card/CardContent.kt
@@ -0,0 +1,169 @@
+package com.example.flashcard.screens.card
+
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.example.flashcard.KeyboardAffiched
+import com.example.flashcard.database.CardViewModel
+import com.example.flashcard.handleAction
+import com.example.flashcard.redBar
+import com.wakaztahir.composejlatex.LatexAlignment
+import com.wakaztahir.composejlatex.latexImageBitmap
+
+@Composable
+fun CardContent(
+    question: String,
+    questionRelativePosition: String,
+    onQuestionChange: (String, String)->Unit,
+    reponse: String,
+    reponseRelativePosition:String,
+    onReponseChange: (String, String)->Unit,
+    cardViewModel: CardViewModel
+){
+    val scrollState = rememberScrollState()
+    val textColor = MaterialTheme.colors.onSurface
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxSize()
+        .verticalScroll(state = scrollState), content = {
+
+        var latexQ by remember { mutableStateOf(redBar + question)}
+        var positionQ by remember { mutableStateOf(0) }
+        var relativePositionQ by remember { mutableStateOf(questionRelativePosition) }
+        var positionInRelativeQ by remember { mutableStateOf(0) }
+
+        var latexR by remember { mutableStateOf(reponse)}
+        var positionR by remember { mutableStateOf(0) }
+        var relativePositionR by remember { mutableStateOf(reponseRelativePosition) }
+        var positionInRelativeR by remember { mutableStateOf(0) }
+
+        val context = LocalContext.current
+        var focus by remember { mutableStateOf("Question")}
+
+        var  imageBitmapQ by remember {
+            mutableStateOf(latexImageBitmap(context, latexQ, alignment = LatexAlignment.Start, color = textColor))
+        }
+        var  imageBitmapR by remember {
+            mutableStateOf(latexImageBitmap(context, latexR, alignment = LatexAlignment.Start, color = textColor))
+        }
+        Card(modifier = Modifier
+            .clip(RoundedCornerShape(15.dp))
+            .fillMaxWidth()
+            .heightIn(50.dp)
+            .clickable {
+                focus = "Question"
+                latexR = latexR.replace(redBar, "")
+                latexQ = latexQ.replace(redBar, "")
+                latexQ = latexQ.substring(0, positionQ) + redBar + latexQ.substring(positionQ)
+            },
+        ) {
+            Image(modifier = Modifier.background(MaterialTheme.colors.onSecondary),
+                bitmap = imageBitmapQ,
+                contentDescription = null
+            )
+        }
+        Card(modifier = Modifier
+            .clip(RoundedCornerShape(15.dp))
+            .fillMaxWidth()
+            .heightIn(50.dp)
+            .clickable {
+                focus = "Reponse"
+                latexQ = latexQ.replace(redBar, "")
+                latexR = latexR.replace(redBar, "")
+                latexR = latexR.substring(0, positionR) + redBar + latexR.substring(positionR)
+            },
+        ) {
+            Image(modifier = Modifier.background(MaterialTheme.colors.onSecondary),
+                bitmap = imageBitmapR,
+                contentDescription = null
+            )
+        }
+
+        KeyboardAffiched(cardViewModel = cardViewModel, onKeyPressed = {text:String, taille:String->
+            if(focus == "Question") {
+                latexQ = latexQ.replace(redBar, "")
+
+                if (text.contains("{")) {
+                    latexQ = latexQ.substring(0, positionQ) + "${text.substring(0, taille[0].code - 48)}$redBar${text.substring(taille[0].code - 48)}" + latexQ.substring(positionQ)
+                    positionQ += taille[0].code - 48
+                }
+                else {
+                    latexQ = latexQ.substring(0, positionQ) + "$text$redBar" + latexQ.substring(positionQ)
+                    positionQ += taille[0].code - 48
+                }
+
+                relativePositionQ = relativePositionQ.substring(0, positionInRelativeQ) + taille + relativePositionQ.substring(positionInRelativeQ)
+                positionInRelativeQ += 1
+                onReponseChange(latexQ.replace(redBar, ""), relativePositionQ)
+            }
+            else if(focus == "Reponse") {
+                latexR = latexR.replace(redBar, "")
+
+                if (text.contains("{")) {
+                    latexR = latexR.substring(0, positionR) + "${text.substring(0, taille[0].code - 48)}$redBar${text.substring(taille[0].code - 48)}" + latexR.substring(positionR)
+                    positionR += taille[0].code - 48
+                }
+                else {
+                    latexR = latexR.substring(0, positionR) + "$text$redBar" + latexR.substring(positionR)
+                    positionR += taille[0].code - 48
+                }
+
+                relativePositionR = relativePositionR.substring(0, positionInRelativeR) + taille + relativePositionR.substring(positionInRelativeR)
+                positionInRelativeR += 1
+                onReponseChange(latexR.replace(redBar, ""), relativePositionR)
+            }
+        },
+            onActionPress = { action: String ->
+                if(focus == "Question"){
+                    handleAction(
+                        action = action,
+                        pos = positionQ,
+                        relPos = relativePositionQ,
+                        positionInRel = positionInRelativeQ,
+                        lat = latexQ,
+                        result = { pos, relPos, posInRel, lat ->
+                            positionQ = pos
+                            relativePositionQ = relPos
+                            positionInRelativeQ = posInRel
+                            latexQ = lat
+                        }
+                    )
+                }
+                else if(focus == "Reponse"){
+                    handleAction(
+                        action = action,
+                        pos = positionR,
+                        relPos = relativePositionR,
+                        positionInRel = positionInRelativeR,
+                        lat = latexR,
+                        result = { pos, relPos, posInRel, lat ->
+                            positionR = pos
+                            relativePositionR = relPos
+                            positionInRelativeR = posInRel
+                            latexR = lat
+                        }
+                    )
+                }
+            }
+        )
+        kotlin.runCatching {
+            latexImageBitmap(context = context, latexR,alignment = LatexAlignment.Start, color = textColor) }
+            .getOrNull()?.let { bitmap ->
+                imageBitmapR = bitmap
+            }
+        kotlin.runCatching {
+            latexImageBitmap(context = context, latexQ,alignment = LatexAlignment.Start, color = textColor) }
+            .getOrNull()?.let { bitmap ->
+                imageBitmapQ = bitmap
+            }
+        onReponseChange(latexR.replace(redBar, ""), relativePositionR)
+        onQuestionChange(latexQ.replace(redBar, ""), relativePositionQ)
+    }, verticalArrangement = Arrangement.SpaceBetween)
+}
---
+++ app/src/main/java/com/example/flashcard/screens/list/ListScreen.kt
@@ -0,0 +1,137 @@
+package com.example.flashcard.screens.list
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.example.flashcard.Action
+import com.example.flashcard.R
+import com.example.flashcard.SearchAppBarState
+import com.example.flashcard.database.CardViewModel
+import com.example.flashcard.database.Folder
+
+@ExperimentalFoundationApi
+@ExperimentalMaterialApi
+@Composable
+fun ListScreen(
+    navigateToTaskScreen: (Int) -> Unit,
+    cardViewModel: CardViewModel,
+    navigateToFolderListScreen: (Action)-> Unit,
+    selectedFolder: Folder,
+    navigateToLearningScreen: (Int) -> Unit,
+    navigateToRevisionScreen: (Int) -> Unit
+)
+{
+    LaunchedEffect(key1 = selectedFolder){
+        cardViewModel.getSelectedFolder(selectedFolder.folderId)
+        cardViewModel.getFolderWithCards(selectedFolder.folderId)
+    }
+    val action by cardViewModel.action
+
+    val folderWithCardsList by cardViewModel.getFolderWithCards.collectAsState()
+    val selceF by cardViewModel.selectedFolder.collectAsState()
+    val searchedCard by cardViewModel.searchCards.collectAsState()
+    val searchAppBarState: SearchAppBarState by cardViewModel.searchAppBarState
+    val searchTextState: String by cardViewModel.searchTextState
+
+    val scaffoldState = rememberScaffoldState()
+
+    cardViewModel.handleDatabaseAction(action = action)
+
+Scaffold(
+    scaffoldState = scaffoldState,
+    topBar = {
+        ListAppBar(
+            cardViewModel = cardViewModel,
+            searchAppBarState = searchAppBarState,
+            searchTextState = searchTextState,
+            navigateToFolderListScreen= navigateToFolderListScreen,
+            selectedFolder = selceF,
+            navigateToTaskScreen = navigateToTaskScreen
+        )
+    },
+    content = {
+        ListContent(
+        cards=folderWithCardsList,
+            folder=selceF,
+        navigateToTaskScreen = navigateToTaskScreen,
+        searchedCards = searchedCard,
+        searchAppBarState = searchAppBarState,
+        onSwipeToDelete = {
+            action, card ->
+            cardViewModel.action.value = action
+        },cardViewModel = cardViewModel
+    )},
+    floatingActionButton = {
+        ListFab(onLearnClicked = navigateToLearningScreen, onRevisionClicked = navigateToRevisionScreen, selectedFolder = selectedFolder.folderId)
+    },
+    )
+}
+
+@Composable
+fun ListFab(
+    onLearnClicked: (CardId: Int) -> Unit,
+    onRevisionClicked: (CardId: Int) -> Unit,
+    selectedFolder: Int?
+){
+    var openDialog by remember {
+        mutableStateOf(false)
+    }
+    FloatingActionButton(onClick = {
+        //openDialog = true
+        if (selectedFolder != null) {
+          //  onRevisionClicked(selectedFolder)
+            onLearnClicked(selectedFolder)
+        }
+
+    }) {if(openDialog){
+        Dialog(onDismissRequest = { openDialog = false },
+            properties = DialogProperties(dismissOnClickOutside = true),
+            content = {
+                Surface(modifier = Modifier
+                    .widthIn(180.dp)
+                    .heightIn(180.dp), shape = RectangleShape, border = BorderStroke(1.dp, Color.LightGray)) {
+                Column(modifier = Modifier.fillMaxWidth()){
+                    Text(modifier = Modifier.padding(10.dp), fontSize = 30.sp, text = "Type of learning", textAlign = TextAlign.Left)
+                Text(
+                    modifier = Modifier.padding(10.dp),
+                    text = "Revision help to familiarize with the cards\nLearning is a methods to verify that you now the cards"
+                )
+                Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
+                    OutlinedButton(modifier = Modifier.padding(10.dp),onClick = {
+                        if (selectedFolder != null) {
+                            onLearnClicked(selectedFolder)
+                        }
+                    }) {
+                        Text(text = "Learning")
+                    }
+                    OutlinedButton(modifier = Modifier.padding(10.dp),onClick = {
+                        if (selectedFolder != null) {
+                            onRevisionClicked(selectedFolder)
+                        }
+                    }) {
+                        Text(text = "Revision")
+                    }
+                }
+                }
+                }
+            }
+        )
+    }
+        Icon(
+            imageVector = Icons.Filled.PlayArrow,
+            contentDescription = stringResource(id = R.string.play_button))
+    }
+}
